425b815de208923de990c165eb7ac7aad9e2e88c
David <lcdong@shmtu.edu.cn>
2019-05-14 17:35:41 +0800

    env

--- server/vessel/vessel/settings.py
+++ server/vessel/vessel/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'vessel',
         'USER': 'root',
-        'PASSWORD': 'wode2016',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
